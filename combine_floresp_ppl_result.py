1fee49d2b48f5aed6459529a1520fdc5125bc0bf
Andrea Wen-Yi Wang <ayw255@nyu.edu>
2025-02-23 01:19:56 -0500

    add IP calculations

--- combine_floresp_ppl_result.py
+++ combine_floresp_ppl_result.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+from pandas.errors import ParserError
 langs = pd.read_csv("langs.csv")
 langs = langs[langs["lang_code_flores"].notnull()]
 models = pd.read_csv("models.csv", header=None)
@@ -14,13 +14,27 @@ model_order = models["model_name"]
 df = pd.DataFrame()
 for model_name in models["model_name"]:
     for lang in langs.to_dict(orient="records"):
-        log_file = f"./results/dev_{lang['lang_code_flores']}/{model_name}/logs_2.txt"
+        log_file = f"./floresp_results_v2/dev_{lang['lang_code_flores']}/{model_name}/logs_2.txt"
         try:
             log = pd.read_csv(log_file, sep="\t")
-        except:
+        except FileNotFoundError:
+            print(model_name, lang["lang_code_flores"], "FileNotFoundError")
             continue
-        if len(log) != 997:
+        except ParserError:
+            print(model_name, lang["lang_code_flores"], "ParserError")
             continue
+        else:
+            if log.shape[0] != 997:
+                print(model_name, lang["lang_code_flores"], "Incomplete")
+                continue
+            elif 'nll_sum' not in log.columns:
+                print(model_name, lang["lang_code_flores"], "Missing nll_sum column")
+            elif log[log['nll_sum'].isnull()].shape[0] > 0:
+                print(model_name, lang["lang_code_flores"], "Has Null Values")
+                continue
+            else:
+                print(model_name, lang["lang_code_flores"])
+        log = log.reset_index()
         log["lang_code"] = lang["lang_code_flores"]
         log["lang_name"] = lang["lang_name"]
         log["lang_category"] = lang["lang_category"]
@@ -33,8 +47,23 @@ for model_name in models["model_name"]:
         df = pd.concat((df, log))
 df = df.sort_values(["lang_category", "lang_code"])
 df = df.join(models.set_index(["model_name"]), on='model_name', how='left')
-df['nll_sum'] = np.log(df['ppl']) * df['n_toks']
-df.to_csv("floresp_nll_output.csv", index=False)
+
+# calculate IP
+for base_lang in ['eng_Latn', 'cmn_Hans']:
+    base_lang_result = df[df['lang_code'] == base_lang].copy()
+    base_lang_result = base_lang_result.pivot(index='index', columns='model_name', values='nll_sum')
+    df[f'ip_base_{base_lang}'] = None
+
+    output = pd.DataFrame()
+    for model_name in df['model_name'].unique():
+        for lang_code in df['lang_code'].unique():
+            model_lang_df = df[(df['model_name'] == model_name) & (df['lang_code'] == lang_code)].copy()
+            model_lang_df = model_lang_df.join(base_lang_result, on='index')
+            
+            ip = model_lang_df['nll_sum'] / model_lang_df[model_name]
+            df.loc[(df['model_name'] == model_name) & (df['lang_code'] == lang_code), f'ip_base_{base_lang}'] = ip
+
+    df.to_csv("floresp_nll_output_v2.csv", index=False)
 
 df["tok_efficiency"] = df["n_toks"] / df["n_chars"]
 # df = df[~df['lang_code'].isin(["yue_Hant", "uig_Arab"])]
@@ -46,43 +75,3 @@ g.set_titles(col_template="{col_name}")
 g.set_ylabels("fertility")
 g.tight_layout()
 plt.savefig("./figures/floresp_tok_bar.pdf")
-
-
-# langs = ["kaz_Arab", "kaz_Cyrl"]
-# models = pd.read_csv("models.csv", header=None)
-# models.columns = ["model_type", "model_path"]
-# models["model_name"] = models["model_path"].apply(lambda t: t.split("/")[-1])
-# model_order = models["model_name"]
-# one graph per model
-
-# df = pd.DataFrame()
-# for model_name in models["model_name"]:
-#     for lang in langs:
-#         log_file = f"./results/dev_{lang}/{model_name}/logs_2.txt"
-#         try:
-#             log = pd.read_csv(log_file, sep="\t")
-#         except:
-#             continue
-#         if len(log) != 997:
-#             continue
-#         log["lang_code"] = lang
-#         log["model_name"] = model_name
-
-#         with open(f"./data/floresp-v2.0-rc.2/dev/dev.{lang}", "r") as f:
-#             sents = f.readlines()
-#         n_chars = [len(s) for s in sents]
-#         log["n_chars"] = n_chars
-#         df = pd.concat((df, log))
-# df['nll_sum'] = np.log(df['ppl']) * df['n_toks']
-
-# df["tok_efficiency"] = df["n_toks"] / df["n_chars"]
-# # df = df[~df['lang_code'].isin(["yue_Hant", "uig_Arab"])]
-# g = sns.catplot(df, x="lang_code", y='tok_efficiency', kind="bar", 
-#                 col="model_name", col_wrap=2, col_order = model_order,
-#                 height=2, aspect=3)
-# g.tick_params(axis='x', which='both', labelsize=10)
-# g.set_titles(col_template="{col_name}")
-# g.set_ylabels("fertility")
-# g.tight_layout()
-# plt.savefig("./figures/kaz_tok_bar.pdf")
-
